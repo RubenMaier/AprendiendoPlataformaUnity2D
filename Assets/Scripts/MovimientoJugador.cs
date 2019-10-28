53ec97958dab4b220153f02f31f325a09e6c49bb
RubenMaier <rmaierenzler@est.frba.utn.edu.ar>
2019-10-27 23:04:40 -0300

    cargando juego ya trabajado

---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public Rigidbody2D jugadorRb;
+    public float velocidadCaminata = .5f;
+    public float velocidadSalto = 150;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        jugadorRb.velocity = new Vector2(Input.GetAxis("Horizontal") * velocidadCaminata, jugadorRb.velocity.y);
+        // GeyKey | mientras la tecla este precionada...
+        // GetKeyDown | cuando se toca una tecla...
+        // GetKeyUp | cuando se suelta una tecla precionada ...
+        // Ojo para que sea universal de teclado y joistick hay que usar lo mismo pero con GetButton___
+        if (Input.GetButtonDown("Jump")) // si preciono la tecla Jump... configurada en el Input Managment
+        {
+            jugadorRb.AddForce(Vector2.up * velocidadSalto); // le creo un vector fuerza hacia arriba... Vector2.up es un versor en realidad creo...
+        }
+    }
+}
